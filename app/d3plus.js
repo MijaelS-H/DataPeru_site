ba39bd34f2eed7bb1e64dd897cee48379f2793ef
ffigueroal <felipe.figueroa.lara@gmail.com>
2020-11-23 12:39:15 -0300

    adds d3plus initial config

--- app/d3plus.js
+++ app/d3plus.js
@@ -1,12 +1,438 @@
+import {mean} from "d3-array";
+import {formatAbbreviate} from "d3plus-format";
+import colors from "../static/data/colors.json";
+import styles from "style.yml";
+
+const typeface = "'Fira Sans Extra Condensed', sans-serif";
+const defaultFontColor = styles["dark-1"];
+const headingFontColor = styles["dark-3"];
+const fontSizeSm = 12;
+const fontSizeMd = 14;
+const fontSizeLg = 16;
+const labelPadding = 5;
+const shapeLegend = 25;
+
+const icons = ["Continente", "Capítulo"];
+
+const getTooltipTitle = (d3plusConfig, d) => {
+  const len = d3plusConfig._groupBy.length;
+  const parentName = d3plusConfig._groupBy[0](d);
+  let parent = Object.entries(d).find(h => h[1] === parentName) || [undefined];
+  let parentId = parent[0];
+  if (parentId.includes(" ID")) {
+    parentId = parentId.slice(0, -3);
+    parent = Object.entries(d).find(h => h[0] === parentId) || [undefined];
+  }
+  const itemName = d3plusConfig._groupBy[len - 1](d);
+  let item = Object.entries(d).find(h => h[1] === itemName) || [undefined];
+  let itemId = item[0];
+  if (itemId.includes(" ID")) {
+    itemId = itemId.slice(0, -3);
+    item = Object.entries(d).find(h => h[0] === itemId) || [undefined];
+  }
+
+  return {item, itemId, parent, parentId};
+};
+
+const growthPct = d => `${formatAbbreviate(d * 100)}%`;
+const pesoPE = d => `S/ ${formatAbbreviate(d * 100)}`;
+
+export const findColorV2 = (key, d) => {
+  if (key === "Pais") {
+    if (!Array.isArray(d["Pais ID"])) return "transparent";
+    else return colors.Continente[d["Continente ID"]];
+  }
+  const id = d[`${key} ID`];
+
+  const palette = colors[key];
+  return palette ? colors[key][id] || colors[key][d[key]] || styles["gpe-red-1"] : styles["gpe-red-1"];
+};
+
+// Tooltip title
+export const tooltipTitle = (bgColor, imgUrl, title) => {
+  let tooltip = "<div class='d3plus-tooltip-title-wrapper'>";
+  if (imgUrl) {
+    tooltip += `<div class="icon" style="background-color: ${bgColor}"><img src="${imgUrl}" /></div>`;
+  }
+  tooltip += `<div class="title"><span>${title}</span></div>`;
+  tooltip += "</div>";
+  return tooltip;
+};
+
+export const findIconV2 = (key, d) => {
+  // const options = {2: "export", 1: "import"};
+  // console.log(key, d);
+  if (key === "Pais") {
+    const icon = (key === "Pais" && Array.isArray(d["Pais ID"])) ? d["Continente ID"] : d[`${key} ID`];
+    return `/icons/visualizations/Pais/country_${icon}.png`;
+  }
+  const icon = key;
+  const iconID = d[`${key} ID`];
+
+  return icons.includes(icon)
+    ? `/icons/visualizations/${icon}/png/white/${iconID}.png`
+    : undefined;
+};
+
+/** default x/y axis styles */
+const axisConfig = {
+  // main bar lines
+  maxSize: 100,
+  barConfig: {
+    stroke: "transparent"
+  },
+  // secondary grid lines
+  gridConfig: {
+    stroke: "#cccccc",
+    strokeWidth: 1
+    // opacity: 0.5
+  },
+  locale: "es-MX",
+  // axis title labels
+  titleConfig: {
+    fontFamily: () => typeface,
+    fontSize: () => fontSizeLg,
+    fontColor: headingFontColor
+  },
+  // value labels
+  shapeConfig: {
+    labelConfig: {
+      labelRotation: false,
+      fontColor: headingFontColor,
+      fontFamily: () => typeface,
+      fontSize: () => fontSizeMd
+    }
+  },
+  // death to ticks
+  tickSize: 0
+};
+
 /**
   The object exported by this file will be used as a base config for any
   d3plus-react visualization rendered on the page.
 */
-
 export default {
-  shapeConfig: {
+  // global defaults
+  aggs: {
+    "Trade Flow ID": mean,
+    "Year": mean
+  },
+  locale: "es-CL",
+  xConfig: axisConfig,
+  yConfig: {...axisConfig, scale: "auto"},
+  y2Config: {...axisConfig, scale: "auto"},
+  barPadding: 0,
+  groupPadding: 10,
+
+  // legends
+  legendConfig: {
+    label(d) {
+      return "";
+    },
+    shapeConfig: {
+      fill(d) {
+        const item = this._parent._groupBy[0](d);
+        let itemId = Object.entries(d).find(h => h[1] === item)[0];
+        if (itemId.includes(" ID")) itemId = itemId.replace(" ID", "");
+        return findColorV2(itemId, d);
+      },
+      backgroundImage(d, i) {
+        const item = this._parent._groupBy[0](d);
+        let itemId = Object.entries(d).find(h => h[1] === item)[0];
+        if (itemId.includes(" ID")) itemId = itemId.replace(" ID", "");
+        return findIconV2(itemId, d);
+      },
+      borderRadius: 0,
+      width: shapeLegend,
+      height: shapeLegend
+    },
     labelConfig: {
-      fontFamily: "Roboto"
+      fontColor: defaultFontColor,
+      fontSize: () => fontSizeLg,
+      fontFamily: () => typeface
+    },
+    stroke: "transparent"
+  },
+
+  // color scale
+  colorScaleConfig: {
+    scale: "quantile",
+    axisConfig: {
+      labelOffset: true,
+      labelRotation: false,
+      locale: "es-CL",
+      shapeConfig: {
+        height: 50,
+        labelConfig: {
+          fontSize: () => fontSizeLg,
+          fontColor: headingFontColor,
+          fontFamily: () => typeface
+        }
+      },
+      titleConfig: {
+        fontFamily: () => typeface,
+        fontColor: headingFontColor
+      },
+      // death to ticks
+      tickSize: 0,
+      tickFormat: d => formatAbbreviate(d),
+      barConfig: {
+        stroke: "transparent"
+      },
+      rectConfig: {
+        stroke: "transparent"
+      }
+    },
+    color: [
+      "#84F0EE",
+      "#4FBEBC",
+      "#008E8D",
+      "#006160",
+      "#005253"
+    ],
+    legendConfig: {
+      shapeConfig: {
+        height: shapeLegend,
+        width: shapeLegend,
+        stroke: "transparent"
+      }
+    },
+    rectConfig: {
+      rx: 0,
+      ry: 0,
+      borderRadius: 0,
+      stroke: "transparent"
     }
-  }
+  },
+  legendTooltip: {
+    title(d) {
+      const {item, parent, parentId} = getTooltipTitle(this, d);
+      const title = Array.isArray(item[1]) ? `${parent[1] || "Values"}` : item[1];
+      const itemBgImg = parentId;
+      const bgColor = findColorV2(itemBgImg, d);
+      const imgUrl = findIconV2(itemBgImg, d);
+      return tooltipTitle(bgColor, imgUrl, title);
+    },
+    tbody: []
+  },
+
+  // geomaps
+  ocean: "transparent",
+  tiles: false,
+  pointSizeMin: 1,
+  pointSizeMax: 7,
+  zoomScroll: false,
+
+  // various visualizations
+  shapeConfig: {
+    // labels
+    fontFamily: () => typeface,
+    labelConfig: {
+      fontFamily: () => typeface,
+      fontMax: 32,
+      padding: 10
+    },
+    // stacked area
+    Area: {
+      labelConfig: {
+        fontColor: styles.white,
+        fontFamily: () => typeface
+        // fontMax: fontSizeLg,
+        // fontMin: fontSizeSm
+      },
+      strokeWidth: d => 1
+    },
+    Bar: {
+      labelConfig: {
+        fontSize: () => 16,
+        fontFamily: () => typeface
+      },
+      textAlign: "left",
+      stroke: "transparent",
+      strokeWidth: d => 1
+    },
+    // line charts
+    Line: {
+      curve: "monotoneX",
+      labelConfig: {
+        fontStrokeWidth: d => 0.5,
+        fontWeight: 600,
+        fontFamily: () => styles["base-font-stack-condensed"],
+        padding: 3
+      },
+      stroke(d) {
+        if (this && this._groupBy) {
+          const item = this._groupBy[0](d);
+          let itemId = Object.entries(d).find(h => h[1] === item)[0];
+          if (itemId.includes(" ID")) itemId = itemId.replace(" ID", "");
+          return findColorV2(itemId, d);
+        }
+        return undefined;
+      },
+      strokeWidth: 3,
+      strokeLinecap: "round"
+    },
+    // scatter plots
+    Circle: {
+      // fill: d => {
+      //   if (d["Trade Value RCA"]) {
+      //     return d["Trade Value RCA"] > 1 ? findColor(d) : "#b1bac6";
+      //   }
+      //   return "#b1bac6";
+      // },
+      // stroke: "#aaaaaa",
+      strokeWidth: 1
+    },
+    fill(d) {
+      if (this && this._groupBy) {
+        const parentName = this._groupBy[0](d);
+        if (parentName) {
+          let parent = Object.entries(d).find(h => h[1] === parentName) || [undefined];
+          let parentId = parent[0];
+          if (parentId.includes(" ID")) {
+            parentId = parentId.slice(0, -3);
+            parent = Object.entries(d).find(h => h[0] === parentId) || [undefined];
+          }
+
+          const bgColor = findColorV2(parentId, d);
+          return bgColor;
+        }
+        else return "green";
+      }
+      else {
+        return "transparent";
+      }
+    }
+  },
+
+  // timelines
+  timelineConfig: {
+    // handle
+    handleConfig: {
+      width: 9,
+      fill: styles["accent-dark"]
+    },
+    tickFormat: d => {
+      d = d.toString().includes("Q") ? d.toString().replace("Q", "0") : d;
+      const latest = new Date(d);
+      const id = latest.getFullYear();
+      const month = latest.getUTCMonth() + 1;
+      const day = latest.getDate();
+
+      const tickString = id.toString();
+      const len = tickString.length;
+
+      let label = "";
+
+      if (month === 1 && id < 20000) {
+        label = id;
+      }
+
+      else if (len === 5) {
+        // ${tickString.slice(0, 4)}-
+        const quarter = tickString.slice(4, 5);
+        label = quarter === "1" ? `${tickString.slice(0, 4)}` : `Q${quarter}`;
+        // ${quarter}${tickString.slice(0, 4)}
+      }
+      else if (len === 6) {
+        label = `${tickString.slice(0, 4)}/${tickString.slice(4, 6)}/01`;
+      }
+      else {
+        label = latest;
+      }
+      return label;
+    },
+    // button stuff
+    brushing: false,
+    buttonBehavior: "buttons",
+    buttonHeight: 20,
+    buttonWidth: 200,
+    buttonPadding: 5,
+    // selected range
+    selectionConfig: {
+      "height": 8,
+      "fill": styles["accent-dark"],
+      "fill-opacity": 0.25,
+      "transform": "translate(0, 2)"
+    },
+    // main horizontal bar line
+    barConfig: {
+      stroke: styles["light-3"],
+      opacity: 0.5
+    },
+    shapeConfig: {
+      // ticks and/or button bg
+      fill: styles["light-3"],
+      stroke: "none",
+      // label and/or button text
+      labelConfig: {
+        fontColor(d) {
+          const n = parseInt(d.text, 10);
+          return defaultFontColor;
+        },
+        fontFamily: () => typeface,
+        fontSize: () => fontSizeSm,
+        lineHeight: () => fontSizeLg,
+        padding: 0
+      }
+    },
+    labelRotation: false,
+    padding: 0
+  },
+
+  // tooltips
+  tooltipConfig: {
+    background: styles.white,
+    footerStyle: {
+      "color": headingFontColor,
+      "fontFamily": () => typeface,
+      "font-size": fontSizeSm,
+      "padding-top": "5px",
+      "text-align": "center"
+    },
+    title(d) {
+      const {item, itemId, parent, parentId} = getTooltipTitle(this, d);
+      const aggregated = Array.isArray(parent[1]) ? "Valores" : parent[1];
+      const title = Array.isArray(item[1]) ? `Otros ${aggregated || "Valores"}` : item[1];
+      const itemBgImg = ["Pais"].includes(itemId) ? itemId : parentId;
+      let bgColor = findColorV2(itemBgImg, d);
+      let imgUrl = findIconV2(itemBgImg, d);
+      if (parentId === "type" && ["Perú"].includes(title)) {
+        imgUrl = "/icons/visualizations/Pais/country_per.png";
+        bgColor = undefined;
+      }
+      return tooltipTitle(bgColor, imgUrl, title);
+    },
+    tbody(d) {
+      const output = [];
+      if (d.Quarter) {
+        output.push(["Quarter", d.Quarter]);
+      }
+      return output;
+    }
+  },
+  totalConfig: {
+    locale: "es-CL",
+    fontSize: () => fontSizeMd
+  },
+  noDataHTML: "<img src='/icons/no-data.png' />",
+  loadingHTML: `<div style="left: 50%; top: 50%; position: absolute; transform: translate(-50%, -50%);">
+      <svg class="cp-viz-spinner" width="60px" height="60px" viewBox="0 0 317 317" xmlns="http://www.w3.org/2000/svg">
+        <path class="outer" d="M16.43 157.072c0 34.797 12.578 66.644 33.428 91.277l-11.144 11.141c-23.673-27.496-37.992-63.283-37.992-102.418 0-39.133 14.319-74.921 37.992-102.423l11.144 11.144c-20.85 24.63-33.428 56.481-33.428 91.279z"/>
+        <path class="outer" d="M157.793 15.708c34.798 0 66.648 12.58 91.28 33.427l11.143-11.144c-27.502-23.676-63.29-37.991-102.423-37.991-39.132 0-74.919 14.315-102.422 37.991l11.148 11.144c24.627-20.847 56.477-33.427 91.274-33.427"/>
+        <path class="outer" d="M299.159 157.072c0 34.797-12.578 66.644-33.43 91.277l11.145 11.141c23.674-27.496 37.992-63.283 37.992-102.418 0-39.133-14.318-74.921-37.992-102.423l-11.145 11.144c20.852 24.63 33.43 56.481 33.43 91.279"/>
+        <path class="outer" d="M157.793 298.432c-34.797 0-66.647-12.574-91.274-33.424l-11.148 11.138c27.503 23.682 63.29 37.997 102.422 37.997 39.133 0 74.921-14.315 102.423-37.997l-11.143-11.138c-24.632 20.85-56.482 33.424-91.28 33.424"/>
+        <path class="middle" d="M226.59 61.474l-7.889 13.659c24.997 18.61 41.184 48.382 41.184 81.94 0 33.555-16.187 63.329-41.184 81.936l7.889 13.664c29.674-21.394 49.004-56.23 49.004-95.6 0-39.373-19.33-74.21-49.004-95.599"/>
+        <path class="middle" d="M157.793 259.169c-52.398 0-95.553-39.485-101.399-90.317h-15.814c5.912 59.524 56.131 106.018 117.213 106.018 17.26 0 33.633-3.742 48.404-10.406l-7.893-13.672c-12.425 5.38-26.114 8.377-40.511 8.377"/>
+        <path class="middle" d="M157.793 54.976c14.397 0 28.086 2.993 40.511 8.371l7.893-13.667c-14.771-6.669-31.144-10.412-48.404-10.412-61.082 0-111.301 46.493-117.213 106.021h15.814c5.846-50.831 49.001-90.313 101.399-90.313"/>
+        <path class="inner" d="M95.371 164.193c-3.476-30.475 15.471-58.324 43.723-67.097l-1.804-15.842c-36.899 9.931-61.986 45.602-57.524 84.719 4.461 39.115 36.934 68.219 75.122 69.584l-1.806-15.838c-29.504-2.186-54.235-25.054-57.711-55.526"/>
+        <path class="inner" d="M162.504 94.425c29.508 2.185 54.235 25.053 57.711 55.529 3.476 30.469-15.466 58.319-43.724 67.096l1.806 15.834c36.898-9.927 61.986-45.598 57.525-84.712-4.461-39.117-36.936-68.223-75.125-69.588l1.807 15.841z"/>
+      </svg>
+      <strong>Cargando...</strong>
+      <sub style="bottom: 0; display: block; line-height: 1; margin-top: 5px;">
+        <a style="color: inherit;" href="https://www.datawheel.us/" target="_blank">
+          Desarrollado por Datawheel
+        </a>
+      </sub>
+    </div>`
 };
