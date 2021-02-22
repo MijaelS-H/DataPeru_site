aa868eed31e3eb03f7cd4d171d6a1052a73aabeb
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-02-22 11:45:33 -0300

    modifies randomTiles api

--- api/randomTiles.js
+++ api/randomTiles.js
@@ -30,48 +30,71 @@ module.exports = function(app) {
       axios.get(nacionAPI).then(response => {
         const {data: nacionList} = response.data;
 
-        return nacionList.forEach(d => d.slug = "geo");
+        nacionList.forEach(d => d.slug = "geo");
+
+        return nacionList;
       }),
       axios.get(departmentAPI).then(response => {
         const {data: departmentList} = response.data;
 
-        return departmentList.forEach(d => d.slug = "geo");
+        departmentList.forEach(d => d.slug = "geo");
+
+        return departmentList;
       }),
       axios.get(provinceAPI).then(response => {
         const {data: provinceList} = response.data;
 
-        return provinceList.forEach(d => d.slug = "geo");
+        provinceList.forEach(d => d.slug = "geo");
+
+        return provinceList;
       }),
       axios.get(districtAPI).then(response => {
         const {data: districtList} = response.data;
 
-        return districtList.forEach(d => d.slug = "geo");
+        districtList.forEach(d => d.slug = "geo");
+
+        return districtList;
       }),
       axios.get(sectionAPI).then(response => {
         const {data: sectionList} = response.data;
 
-        return sectionList.forEach(d => d.slug = "industry");
+        sectionList.forEach(d => d.slug = "industry");
+
+        return sectionList;
       }),
       axios.get(divisionAPI).then(response => {
         const {data: divisionList} = response.data;
 
-        return divisionList.forEach(d => d.slug = "industry");
+        divisionList.forEach(d => d.slug = "industry");
+
+        return divisionList;
       }),
       axios.get(citeAPI).then(response => {
         const {data: citeList} = response.data;
 
-        return citeList.forEach(d => d.slug = "cite");
+        citeList.forEach(d => d.slug = "cite");
+
+        return citeList;
       })
     ]).then(results => {
       const [nation, departments, provinces, districts, sections, divisions, cites] = results;
 
-      const geoEntities = nation.push(departments, provinces, districts);
-      const industryEntities = sections.push([divisions]);
-      const citeEntities = cites;
+      nation.push(departments, provinces, districts);
+      const geoEntities = nation.flat();
+
+      sections.push(divisions);
+      let industryEntities = sections.flat();
+      industryEntities = industryEntities.filter(d => ![
+        "O", "T", "U", "Z", "0", "1", "2", "3", "4", "5", "6", "8", "9", "12", "35", "36", "37", "38", "39",
+        "41", "42", "43", "45", "46", "47", "49", "50", "51", "52", "53", "55", "56", "58", "59", "60", "63",
+        "64", "65", "66", "68", "75", "77", "78", "79", "80", "81", "82", "84", "85", "86", "87", "88", "90",
+        "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"].includes(d.ID));
+
+      console.log(industryEntities);
 
       const randomGeoEntity = geoEntities[Math.floor(Math.random() * geoEntities.length)];
       const randomIndustryEntity = industryEntities[Math.floor(Math.random() * industryEntities.length)];
-      const randomCiteEntity = citeEntities[Math.floor(Math.random() * citeEntities.length)];
+      const randomCiteEntity = cites[Math.floor(Math.random() * cites.length)];
 
       res.json({status: "ok", data: [randomGeoEntity, randomIndustryEntity, randomCiteEntity]});
     }).catch(err => {
