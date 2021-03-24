9b0468de87d26a4bb289ebef3bd135b5ec10b656
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-03-24 15:20:12 -0300

    updates favicon route

--- app/helmet.js
+++ app/helmet.js
@@ -1,13 +1,13 @@
 export default {
   link: [
-    {rel: "icon", href: "favicon.svg"},
+    {rel: "icon", href: "/images/favicon.svg"},
     {rel: "preconnect", href: "https://fonts.gstatic.com/", crossorigin: ""},
     {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Cabin:wght@200;300;400;500;600;700&family=Open+Sans:wght@200;300;400;500;600;700&display=swap"}
   ],
   meta: [
     {charset: "utf-8"},
     {"http-equiv": "X-UA-Compatible", "content": "IE=edge"},
-    {name: "description", content: ""},
+    {name: "description", content: "Descubra los principales indicadores económicos y sectoriales del Perú"},
     {name: "viewport", content: "width=device-width, initial-scale=1"},
     {name: "mobile-web-app-capable", content: "yes"},
     {name: "apple-mobile-web-app-capable", content: "yes"},
