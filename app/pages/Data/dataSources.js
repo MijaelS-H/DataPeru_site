15999faf25bef1415ed38d9707d68040a1a4bf6f
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-04-06 16:45:16 -0400

    modifies data page

--- app/pages/Data/dataSources.js
+++ app/pages/Data/dataSources.js
@@ -1,29 +1,368 @@
 export const dataSources = [
   {
-    name: "Consejo Nacional de Ciencia, Tecnología e Innovación Tecnológica"
+    id: 1,
+    name: "Consejo Nacional de Ciencia, Tecnología e Innovación Tecnológica",
+    shortname: "CONCYTEC",
+    summary: [
+      "El CONCYTEC es el órgano rector del SINACYT, encargado de dirigir, fomentar, coordinar, supervisar y evaluar las acciones de Estado en todo el país en el ámbito de la ciencia, tecnología e innovación tecnológica; orienta las acciones del sector privado; y ejecuta acciones de soporte que impulsen el desarrollo científico y tecnológico del país.",
+      "Consejo Nacional de Ciencia, Tecnología e Innovación Tecnológica (CONCYTEC) es una entidad adscrita a Presidencia del Consejo de Ministros."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Instituto Nacional de Estadística e Informática"
+    id: 2,
+    name: "Instituto Nacional de Estadística e Informática",
+    shortname: "INEI",
+    summary: [
+      "El INEI es un oragnísmo técnico especializado responsable de producir y difundir información estadística oficial que el país necesita con calidad, oportunidad y cobertura requerida, con el propósito de contribuir al diseño, monitoreo y evaluación de políticas públicas y al proceso de toma de decisiones de los agentes socioeconómicos, el sector público y la comunidad en general.",
+      "Instituto Nacional de Estadística e Informática (INEI) es una entidad adscrita a Presidencia del Consejo de Ministros."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      },
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Instituto Tecnológico de la Producción"
+    id: 3,
+    name: "Instituto Tecnológico de la Producción",
+    shortname: "ITP",
+    summary: [
+      "El ITP es un organismo técnico especializado que contribuye a la mejora de la productividad, calidad y rentabilidad de las empresas a través de la provisión de servicios de investigación, desarrollo, innovación, adaptación, transformación y transferencia tecnológica ambientalmente sostenibles y accesibles, en coordinación con entidades de soporte productivo y del ecosistema de CTI (Ciencia Tecnología e Innovación).",
+      "A través de los servicios de los CITE (Centros de Innovación Productiva y Transferencia Tecnológica), asegura el cumplimiento de las normas técnicas, las buenas prácticas y estándares de calidad que permiten a las empresas desarrollar productos de mejor calidad y aprovechar las oportunidades de los mercados a nivel nacional e internacional.",
+      "Cada CITE es un punto de encuentro entre el Estado, la academia y el sector privado que articula con el resto de elementos del sistema de innovación de las cadenas productivas.",
+      "De esta manera, ITP aporta a la visión del Ministerio de la Producción (PRODUCE), la cual busca que las empresas produzcan y accedan a mercados de manera sostenible, competitiva y con altos niveles de productividad.",
+      "Instituto Tecnológico de la Producción (ITP) es una entidad adscrita a Presidencia del Consejo de Ministros."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Ministerio de Cultura"
+    id: 4,
+    name: "Ministerio de Cultura",
+    shortname: "MINCUL",
+    summary: [
+      "El MINCUL es el órgano del Poder Ejecutivo del Perú encargado del sector de cultura.",
+      "Sus principales funciones son formular, ejecutar y establecer estrategias de promoción cultural de manera inclusiva y accesible, realizar acciones de conservación y protección del patrimonio cultural, fomentar toda forma de expresiones artísticas, convocar y reconocer el mérito de quienes aporten al desarrollo cultural del país, planificar y gestionar con todos los niveles de gobierno actividades que permitan el desarrollo de los pueblos amazónicos, andinos y afroperuanos, todo ello propiciando el fortalecimiento de la ciudadanía e identidad cultural y abriendo espacios de participación de todas las culturas, mediante lo cual se democratiza la cultura para acercarla al ciudadano."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Ministerio de Desarrollo Agrario y Riego"
+    id: 5,
+    name: "Ministerio de Desarrollo Agrario y Riego",
+    shortname: "MIDAGRI",
+    summary: [
+      "El MIDAGRI es el organismo público rector en materia agragia, encargado de desarrollar estrategias alineadas a proveer de bienes y servicios públicos de calidad a miles de familias del país, las mismas que son desarrolladas en concordancia con la Política Nacional Agraria, que define orientaciones de mediano y largo plazo, a favor del desarrollo sostenible de la agricultura, priorizando la agricultura familiar, y permitiendo activar el desarrollo y la inclusión social en beneficio de la población rural, contribuyendo además a la seguridad alimentaria y nutricional en el Perú.",
+      "Adicionalmente, el MIDAGRI actúa como agente regulador en el uso de recursos forestales y de fauna silvestre, fuentes de agua naturales, estado fito y zoosanitario, actividad agragia y ganadera, y como agente promovedor de la calidad de productos relacionados, fomentando el acceso de productos a mercados nacionales e internacionales bajo políticas de seguridad alimentaria y de desarrollo económico ambientalmente amigable."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Ministerio de Economía y Finanzas"
+    id: 6,
+    name: "Ministerio de Economía y Finanzas",
+    shortname: "MEF",
+    summary: [
+      "El MEF corresponde al sector público que impulsa el crecimiento económico sostenido, que contribuye a una mejor calidad de vida de los peruanos, garantizando una política fiscal responsable y transparente, en el marco de la estabilidad macroeconómica.",
+      "Su misión es armonizar la política económica y financiera, a través de la transparencia y responsabilidad fiscal, contribuyendo al crecimiento económico sostenido del país.",
+      "Sus principales objetivos están orientados hacia la consolidación del equlibrio y sostenibilidad fiscal, el mejoramiento del nivel de estabilidad de los ingresos públicos, fomentar una apertura económica y una armonización del mercado de bienes y servicios, mejorar la calidad del gasto público en los diversos niveles de gobierno, optimizar la transparencia y rendición de cuentas en el sector público y modernizar la gestión institucional del Ministerio."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Ministerio de la Producción"
+    id: 7,
+    name: "Ministerio de la Producción",
+    shortname: "PRODUCE",
+    summary: [
+      "El PRODUCE es el organismo público encargado de formular, diseñar, ejecutar y supervisar la política nacional y sectorial de la Pesca, Acuicultura, MYPE e Industria.",
+      "Ejerce competencia en el ordenamiento pesquero y acuícola, pesquería industrial, acuicultura de mediana y gran empresa, normalización industrial; así como de productos fiscalizados, innovación productiva y transferencia tecnológica.",
+      "Provee el desarrollo de manera armónica de los ecosistemas productivos de pesquería, acuicultura, industria, micro, pequeña, mediana y gran empresa, comercio interno y cooperativas.",
+      "Revalora la pesca artesanal, promueve la producción para consumo humano directo y busca mejorar las condiciones de vida del pescador.",
+      "El sector está integrado por el Fondo Nacional de Desarrollo Pesquero (FONDEPES); Instituto del Mar del Perú (IMARPE); Organismo de Sanidad Pesquera (SANIPES); Instituto Tecnológico de la Producción (ITP); Instituto Nacional de la Calidad (INACAL); Programa Nacional A Comer Pescado, Programa INNÓVATE PERÚ; Programa de Innovación en Pesca y Acuicultura; Programa de Diversificación Productiva, Programa Tu Empresa y las comisiones, iniciativas, proyectos bajo el ámbito de su competencia, responsabilidad política y funcional."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Plataforma de Información Estadística del Sector Cultura"
+    id: 8,
+    name: "Plataforma de Información Estadística del Sector Cultura",
+    shortname: "INFOCULTURA",
+    summary: [
+      "INFOCULTURA es una herramienta que permite un mejor conocimiento del quehacer del sector cultura a través de un conjunto de indicadores cuya información se encuentra desagregada a nivel de departamento, provincia y distrito.",
+      "De este modo, INFOCULTURA presenta información actualizada respecto a los bienes arqueológicos e históricos integrantes del patrimonio cultural de la nación, las expresiones culturales declaradas patrimonio cultural de la nación, la cantidad de público asistente a las presentaciones de los elencos nacionales, las personalidades que han sido reconocidas por su aporte a la cultura, los procesos de consulta previa en curso, los pueblos indígenas en aislamiento y contacto inicial, el registro de traductores e intérpretes, así como información sociodemográfica sobre la población afrodescendiente, entre otros aspectos."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   },
   {
-    name: "Superintendencia Nacional de Aduanas y de Administración Tributaria"
+    id: 9,
+    name: "Superintendencia Nacional de Aduanas y de Administración Tributaria",
+    shortname: "SUNAT",
+    summary: [
+      "La SUNAT administra los tributos del gobierno nacional (con excepción de las municipales) y facilita el comercio exterior promoviendo el cumplimiento tributario y aduanero mediante el uso de la tecnología y el fortalecimiento de la conciencia tributaria. SUNAT trabaja con un equipo humano íntegro, unificado, competente y comprometido con el país para brindar un servicio de excelencia.",
+      "Superintendencia Nacional de Aduanas y de Administración Tributaria (SUNAT) es una entidad adscrita a Ministerio de Economía y Finanzas."
+    ],
+    datasets: [
+      {
+        name: "Censo Nacional de Investigación y Desarrollo",
+        link: "#",
+        indicators: [
+          {
+            name: "Número de Centros de Investigación según clasificación",
+            link: "#"
+          },
+          {
+            name: "Gasto promedio en I+D en Centros de Investigación",
+            link: "#"
+          },
+          {
+            name: "Centros de Investigación que incurrieron en gastos de I+D",
+            link: "#"
+          },
+          {
+            name: "Total gasto corriente y de inversión en I+D según destino en 2014 y 2015",
+            link: "#"
+          },
+          {
+            name: "Número de proyectos de investigación iniciados en 2014 y 2015, según área de conocimiento",
+            link: "#"
+          }
+        ]
+      }
+    ]
   }
 ];
