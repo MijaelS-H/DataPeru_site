fc2ad080907d5db58c16a9fd28b8c2618bbb2d62
alanramirez <alanramirezb@gmail.com>
2021-04-29 16:08:34 -0400

    fixes scroll bug

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -25,6 +25,7 @@ const Nav = props => {
   useScrollPosition(({prevPos, currPos}) => {
     if (prevPos.y < -20 && currPos.y > -20) setScrolled(false);
     else if (prevPos.y > -20 && currPos.y < -20) setScrolled(true);
+    else if (currPos.y < -20) setScrolled(true);
   });
 
   return (
