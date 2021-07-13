5599799520aa92cfa97aede5e11ea7b58ea4b47f
ffigueroal <felipe.figueroa.lara@gmail.com>
2021-07-13 12:21:31 -0400

    updates footer style

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -30,67 +30,42 @@ class Footer extends React.Component {
             </a>
           </div>
         </div>
-        <div className="footer-logos-support">
+        <div className="footer-logos">
           <div className="footer-logos-left">
-            <div className="footer-logos-team">
-              <span className="footer-logos-header">Desarrollado por:</span>
-              <div className="footer-logos-team-box">
-                <a href="https://www.gob.pe/produce" target="_blank">
-                  <img className="footer-logo-produce" src="/icons/footer/Min_produccion_logo.png" alt="PRODUCE" />
-                </a>
-                <a href="https://www.gob.pe/itp" target="_blank">
-                  <img className="footer-logo-itp" src="/icons/footer/ITP_color.png" alt="ITP"/>
-                </a>
-                <a href="https://bicentenario.gob.pe/" target="_blank">
-                  <img className="footer-logo-bicentenario" src="/icons/footer/logo_bicentenario_peru.png" alt="Bicentenario Perú" />
-                </a>
-              </div>
-            </div>
-            <div className="footer-contact">
-              <img className="footer-contact-icon" src="/icons/footer/mail_icon.svg" />
-              <a className="footer-contact-text" href="mailto:contacto@itp.gob.pe?Subject=ITPProducción">contacto@itp.gob.pe</a>
+            <h4 className="footer-logos-header">Desarrolado por:</h4>
+            <div className="footer-logos-left-grid">
+              <a className="left-item-1" href="https://www.gob.pe/produce" target="_blank">
+                <img className="footer-logo-produce" src="/icons/footer/Min_produccion_logo.png" alt="PRODUCE" />
+              </a>
+              <a className="left-item-2" href="https://www.gob.pe/itp" target="_blank">
+                <img className="footer-logo-itp" src="/icons/homepage/logos_peru-itp.png" alt="ITP" />
+              </a>
+              <a className="left-item-3" href="https://bicentenario.gob.pe/" target="_blank">
+                <img className="footer-logo-bicentenario" src="/icons/footer/logo_bicentenario_peru.png" alt="Bicentenario Perú" />
+              </a>
             </div>
           </div>
           <div className="footer-logos-right">
-            <div className="footer-logos-partners">
-              <span className="footer-logos-header">En coordinación con:</span>
-              <div className="footer-logos-partners-box">
-                <div className="footer-logos-partners-box-top">
-                  <a href="https://www.gob.pe/mef" target="_blank">
-                    <img className="footer-logo-mef" src="/icons/footer/Min_economia_y_finanzas_logo.png" alt="MEF" />
-                  </a>
-                  <a href="https://www.gob.pe/cultura" target="_blank">
-                    <img className="footer-logo-mincul" src="/icons/footer/Min_Cultura_logo.png" alt="MINCUL" />
-                  </a>
-                  <a href="https://www.gob.pe/midagri" target="_blank">
-                    <img className="footer-logo-midagri" src="/icons/footer/Min_Agricultura_logo.png" alt="MIDAGRI" />
-                  </a>
-                </div>
-                <div className="footer-logos-partners-box-bottom">
-                  <a href="https://www.inei.gob.pe/" target="_blank">
-                    <img className="footer-logo-inei" src="/icons/footer/INEI_logo.png" alt="INEI" />
-                  </a>
-                  <a href="https://www.sunat.gob.pe/" target="_blank">
-                    <img className="footer-logo-sunat" src="/icons/footer/logo-blanco-sunat.png" alt="SUNAT" />
-                  </a>
-                  <a href="https://www.gob.pe/concytec" target="_blank">
-                    <img className="footer-logo-concytec" src="/icons/footer/logo-concytec_horizontal-blanco.png" alt="CONCYTEC" />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="footer-logos-support">
-              <div className="footer-logos-support-box">
-                <span className="footer-logos-header">Apoyado por:</span>
-                <div className="footer-logos-support-items">
-                  <a href="https://www.datawheel.us/" target="_blank">
-                    <img className="footer-logo-dw" src="/icons/footer/dw_blanco.svg" alt="Datawheel" />
-                  </a>
-                  <a href="https://www.iadb.org/es" target="_blank">
-                    <img className="footer-logo-bid" src="/icons/footer/BID_logo.png" alt="BID" />
-                  </a>
-                </div>
-              </div>
+            <h4 className="footer-logos-header">En coordinación con:</h4>
+            <div className="footer-logos-right-grid">
+              <a className="right-item-1" href="https://www.gob.pe/mef" target="_blank">
+                <img className="footer-logo-mef" src="/icons/footer/Min_economia_y_finanzas_logo.png" alt="MEF" />
+              </a>
+              <a className="right-item-2" href="https://www.gob.pe/cultura" target="_blank">
+                <img className="footer-logo-mincul" src="/icons/footer/Min_Cultura_logo.png" alt="MINCUL" />
+              </a>
+              <a className="right-item-3" href="https://www.gob.pe/midagri" target="_blank">
+                <img className="footer-logo-midagri" src="/icons/footer/Min_Agricultura_logo.png" alt="MIDAGRI" />
+              </a>
+              <a className="right-item-4" href="https://www.gob.pe/concytec" target="_blank">
+                <img className="footer-logo-concytec" src="/icons/footer/logo-concytec_horizontal-blanco.png" alt="CONCYTEC" />
+              </a>
+              <a className="right-item-5" href="https://www.iadb.org/es" target="_blank">
+                <img className="footer-logo-bid" src="/icons/footer/BID_logo.png" alt="BID" />
+              </a>
+              <a className="right-item-6" href="https://www.datawheel.us/" target="_blank">
+                <img className="footer-logo-dw" src="/icons/footer/dw_blanco.svg" alt="Datawheel" />
+              </a>
             </div>
           </div>
         </div>
